bdeb09a998a60d523b7e22d7cdd9e1ace80c082a
Ubuntu <ubuntu@ip-172-31-11-81.us-west-1.compute.internal>
2024-04-25 18:00:05 +0000

    views have navbar, routes have homepage=products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  root "products#index"                          #main page is products page.
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
