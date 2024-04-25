a4bacf34edfbbabc262559881e2c55854eb86fef
Ubuntu <ubuntu@ip-172-31-11-81.us-west-1.compute.internal>
2024-04-25 17:46:08 +0000

    updated product.rb with def to_s method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    
+    validates :name, :price, presence: true
+    
+
+    def to_s                                    #makes sure product name is always displayed and not object 
+        name 
+    end 
 end
